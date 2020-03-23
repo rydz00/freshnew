475d6a411f82273d1702277ecc71f92b210e62da
robryd <rydz00@gmail.com>
2020-03-23 15:22:16 -0400

    updates to md

--- scripts/mntrydznas.sh
+++ scripts/mntrydznas.sh
@@ -6,8 +6,8 @@ sudo mkdir /mnt/rydznas
 #sudo smbmount //wwp-ldnrr01/Share /mnt/rydznas -o user=,domain=
 
 #CIFS mount
-#sudo mount -t cifs //wwp-ldnrr01/Share /mnt/rydznas -o user=,domain=
+#sudo mount -t cifs //windowshost/Share /mnt/rydznas -o user=,domain=
 #sudo mount -t cifs '\\5.5.5.2\NAS' /mnt/rydznas -o username=,password=,rw,iocharset=utf8,uid=500,gid=50
 
 #nfs mount
-sudo mount.nfs 5.5.5.2:/mnt/md1/NAS /mnt/rydznas
+sudo mount.nfs 5.5.5.2:/mnt/md0/NAS /mnt/rydznas

b57d092c32abd31a9beca33afb9fe72e73876964
robryd <rydz00@gmail.com>
2022-03-22 00:33:06 -0400

    adding linux cifs options

--- scripts/mntanything.sh
+++ scripts/mntanything.sh
@@ -1,8 +1,11 @@
-echo "Tell me the Directory to Create(ex. \Volumes\<newname>; just enter name and I will do the rest"
+#echo "Tell me the Directory to Create(ex. \Volumes\<newname>; just enter name and I will do the rest"
+echo "Tell me the Directory to Create(ex. /mnt/<newname>; just enter name and I will do the rest"
 read a
-/bin/mkdir /Volumes/$a
+#/bin/mkdir /Volumes/$a
+sudo /bin/mkdir /mnt/$a
 echo "Now tell me the path to map to(ex. nas3/users use the slashes as shown)"
 read b
 echo "what is your AD mapping name? do not use US in front of it"
 read user
-/sbin/mount -t smbfs //$user@$b /Volumes/$a
+#/sbin/mount -t smbfs //$user@$b /Volumes/$a
+sudo /usr/bin/mount -t cifs -o username=$user //$b /mnt/$a

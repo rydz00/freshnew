b4787901ab6dcbeea53e85c2fcdf0836b16a911f
robryd <rydz00@gmail.com>
2025-11-11 22:11:06 -0500

    adding roku support

--- media/nasvideosearch.sh
+++ media/nasvideosearch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 filter=$1
 #find $1 -not -path "*/.*/*" -not -name ".*" -not -name "*.db" -not -name "*.pdf" -not -name "*.rar" -not -name "*.[mM]4[vV]" -not -name "*.[nN][fF][oO]" -not -name "*.[mM][pP]4" -not -name "*.[sS][rR][tT]" -not -name "*.[tT][xX][tT]" -not -name "*.[jJ][pP][gG]" -not -name "*.[pP][nN][gG]" -not -type d > ~/rawvideos
-find $1 -not -path "*/.*/*" -not -name ".*" -not -name "*.db" -not -name "*.pdf" -not -name "*.rar" -not -name "*.[nN][fF][oO]" -not -name "*.[sS][rR][tT]" -not -name "*.[tT][xX][tT]" -not -name "*.[jJ][pP][gG]" -not -name "*.[pP][nN][gG]" -not -type d > ~/rawvideos
+find $1 -not -path "*/.*/*" -not -name ".*" -not -name "*.db" -not -name "*.pdf" -not -name "*.rar" -not -name "*.[nN][fF][oO]" -not -name "*.[sS][rR][tT]" -not -name "*.[tT][xX][tT]" -not -name "*.[jJ][pP][gG]" -not -name "*.[pP][nN][gG]" -not -name ".roku.mp4" -not -type d > ~/rawvideos

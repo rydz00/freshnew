e085e48fd72da7f320efc76994f2e8baf3d7954c
rydz00 <rydz00@gmail.com>
2025-12-07 22:34:00 -0500

    modernize rename regex

--- media/removespaces.sh
+++ media/removespaces.sh
@@ -1,3 +1,5 @@
 dir=$1
-find $1 -depth -name "* *" -execdir rename " " "_" "{}" ";"
+#find $1 -depth -name "* *" -execdir rename " " "_" "{}" ";"
+find $1 -depth -name "* *" -execdir rename 's/ /_/g' "{}" \;
 find $1 -depth -name "* *"
+#apt-get install rename
